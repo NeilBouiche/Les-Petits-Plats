5bce3481ccc44166d3f6c3f05a6c499be5c91d09
Neil Bouiche <neil.bouiche@icloud.com>
2022-12-13 12:20:21 +0100

    Barre de recherche principal terminee

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { recipes } from "/data/recipes.js";
-
+// liste des variables a utiliser dans le projet
 let ingredientsList = [];
 let appareilsList = [];
 let ustensilsList = [];
@@ -14,51 +14,50 @@ const customOptionUstensils = document.querySelector(
 );
 const validTagsContainer = document.querySelector(".valid-tags-container");
 const cardsContainer = document.querySelector(".cards-container");
+const mainSearchBar = document.querySelector(".search_bar_input");
+let foundFinal = [];
 
 //  Tag Population --------------------------
 // Boucle pour assigner tout les ingredients au array et supprimer les doublon qui ont le meme index
-for (let i = 0; i < recipes.length; i++) {
-  ingredientsList.push(recipes[i].ingredients[0].ingredient);
-  ingredientsList = ingredientsList.filter(
-    (x, i) => ingredientsList.indexOf(x) === i
-  );
-  appareilsList.push(recipes[i].appliance);
-  appareilsList = appareilsList.filter(
-    (x, i) => appareilsList.indexOf(x) === i
-  );
-  ustensilsList.push(recipes[i].ustensils[0]);
-  ustensilsList = ustensilsList.filter(
-    (x, i) => ustensilsList.indexOf(x) === i
-  );
+function tagsPopulations(data) {
+  for (let i = 0; i < data.length; i++) {
+    ingredientsList.push(data[i].ingredients[0].ingredient);
+    ingredientsList = ingredientsList.filter(
+      (x, i) => ingredientsList.indexOf(x) === i
+    );
+    appareilsList.push(data[i].appliance);
+    appareilsList = appareilsList.filter(
+      (x, i) => appareilsList.indexOf(x) === i
+    );
+    ustensilsList.push(data[i].ustensils[0]);
+    ustensilsList = ustensilsList.filter(
+      (x, i) => ustensilsList.indexOf(x) === i
+    );
+  }
+  ingredientsList.forEach((ingredient) => {
+    const customIngredient = document.createElement("span");
+    customIngredient.classList.add("custom-option");
+    customIngredient.setAttribute("data-value", ingredient);
+    customIngredient.innerHTML = ingredient;
+    customOptionIngredient.appendChild(customIngredient);
+    console.log(customIngredient);
+  });
+  appareilsList.forEach((appareil) => {
+    const customAppareil = document.createElement("span");
+    customAppareil.classList.add("custom-option");
+    customAppareil.setAttribute("data-value", appareil);
+    customAppareil.innerHTML = appareil;
+    customOptionAppareils.appendChild(customAppareil);
+  });
+  ustensilsList.forEach((ustensil) => {
+    const customUstensil = document.createElement("span");
+    customUstensil.classList.add("custom-option");
+    customUstensil.setAttribute("data-value", ustensil);
+    customUstensil.innerHTML = ustensil;
+    customOptionUstensils.appendChild(customUstensil);
+  });
 }
-
-// Creer les nodes des options d'ingredients et les inserer dans le menu
-ingredientsList.forEach((ingredient) => {
-  const customIngredient = document.createElement("span");
-  customIngredient.classList.add("custom-option");
-  customIngredient.setAttribute("data-value", ingredient);
-  customIngredient.innerHTML = ingredient;
-  customOptionIngredient.appendChild(customIngredient);
-});
-
-// Creer les nodes des options d'ingredients et les inserer dans le menu
-appareilsList.forEach((appareil) => {
-  const customAppareil = document.createElement("span");
-  customAppareil.classList.add("custom-option");
-  customAppareil.setAttribute("data-value", appareil);
-  customAppareil.innerHTML = appareil;
-  customOptionAppareils.appendChild(customAppareil);
-});
-
-// Creer les nodes des options d'ustensils et les inserer dans le menu
-ustensilsList.forEach((ustensil) => {
-  const customUstensil = document.createElement("span");
-  customUstensil.classList.add("custom-option");
-  customUstensil.setAttribute("data-value", ustensil);
-  customUstensil.innerHTML = ustensil;
-  customOptionUstensils.appendChild(customUstensil);
-});
-
+tagsPopulations(recipes);
 // Tags Logic -------------------
 // Ouvrir la tag container et inserer l'input de recherche
 for (const selectWrapper of document.querySelectorAll(".select-wrapper")) {
@@ -72,7 +71,6 @@ for (const selectWrapper of document.querySelectorAll(".select-wrapper")) {
     }
   });
 }
-
 // Creation du validTag au dessus du champs de recherche au click de l'element
 for (const option of document.querySelectorAll(".custom-option")) {
   option.addEventListener("click", function () {
@@ -86,14 +84,13 @@ for (const option of document.querySelectorAll(".custom-option")) {
     validTag.append(tagName, tagClose);
     validTagsContainer.appendChild(validTag);
     option.classList.add("disabled");
-    //Suppression du validTaf au click de la croix
+    //Suppression du validTag au click de la croix
     tagClose.addEventListener("click", () => {
       option.classList.remove("disabled");
       validTagsContainer.removeChild(validTag);
     });
   });
 }
-
 // Ferme la tagList et la boite de recherche si on clique autre part
 window.addEventListener("click", function (e) {
   for (const select of document.querySelectorAll(".select")) {
@@ -102,53 +99,83 @@ window.addEventListener("click", function (e) {
     }
   }
 });
-
 // Creation des cards des recettes et leur contenu
-
-for (let i = 0; i < recipes.length; i++) {
-  const card = document.createElement("div");
-  card.classList.add("card");
-  const cardImg = document.createElement("div");
-  cardImg.classList.add("card-img");
-  const cardHead = document.createElement("div");
-  cardHead.classList.add("card-head");
-  const cardTitle = document.createElement("p");
-  cardTitle.classList.add("card-title");
-  cardTitle.innerHTML = recipes[i].name;
-  const cardTimeBox = document.createElement("div");
-  cardTimeBox.classList.add("card-time-box");
-  const cardClock = document.createElement("i");
-  cardClock.classList.add("fa-regular");
-  cardClock.classList.add("fa-clock");
-  const cardTimer = document.createElement("p");
-  cardTimer.classList.add("card-timer");
-  cardTimer.innerHTML = recipes[i].time;
-  const cardInstructions = document.createElement("div");
-  cardInstructions.classList.add("card-instructions");
-  const cardIngredients = document.createElement("div");
-  cardIngredients.classList.add("card-ingredients");
-  recipes[i].ingredients.map((ingredient) => {
-    if (ingredient.quantity) {
-      const ingredientItem = document.createElement("p");
-      ingredientItem.classList.add("ingredient-item");
-      ingredientItem.innerHTML = ingredient.unit
-        ? ingredient.ingredient + ": " + ingredient.quantity + ingredient.unit
-        : ingredient.ingredient + ": " + ingredient.quantity;
-      cardIngredients.appendChild(ingredientItem);
-    } else {
-      const ingredientItem = document.createElement("p");
-      ingredientItem.classList.add("ingredient-item");
-      ingredientItem.innerHTML = ingredient.ingredient;
-      ingredientItem.innerHTML.slice(0, 100) + "...";
-      cardIngredients.appendChild(ingredientItem);
-    }
-  });
-  const cardSteps = document.createElement("p");
-  cardSteps.classList.add("card-steps");
-  cardSteps.innerHTML = recipes[i].description.slice(0, 100) + "...";
-  card.append(cardImg, cardHead, cardInstructions);
-  cardHead.append(cardTitle, cardTimeBox);
-  cardTimeBox.append(cardClock, cardTimer);
-  cardInstructions.append(cardIngredients, cardSteps);
-  cardsContainer.appendChild(card);
+function createCards(data) {
+  for (let i = 0; i < data.length; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const cardImg = document.createElement("div");
+    cardImg.classList.add("card-img");
+    const cardHead = document.createElement("div");
+    cardHead.classList.add("card-head");
+    const cardTitle = document.createElement("p");
+    cardTitle.classList.add("card-title");
+    cardTitle.innerHTML = data[i].name;
+    const cardTimeBox = document.createElement("div");
+    cardTimeBox.classList.add("card-time-box");
+    const cardClock = document.createElement("i");
+    cardClock.classList.add("fa-regular");
+    cardClock.classList.add("fa-clock");
+    const cardTimer = document.createElement("p");
+    cardTimer.classList.add("card-timer");
+    cardTimer.innerHTML = data[i].time + "min";
+    const cardInstructions = document.createElement("div");
+    cardInstructions.classList.add("card-instructions");
+    const cardIngredients = document.createElement("div");
+    cardIngredients.classList.add("card-ingredients");
+    data[i].ingredients.map((ingredient) => {
+      if (ingredient.quantity) {
+        const ingredientItem = document.createElement("p");
+        ingredientItem.classList.add("ingredient-item");
+        ingredientItem.innerHTML = ingredient.unit
+          ? ingredient.ingredient + ": " + ingredient.quantity + ingredient.unit
+          : ingredient.ingredient + ": " + ingredient.quantity;
+        cardIngredients.appendChild(ingredientItem);
+      } else {
+        const ingredientItem = document.createElement("p");
+        ingredientItem.classList.add("ingredient-item");
+        ingredientItem.innerHTML = ingredient.ingredient;
+        ingredientItem.innerHTML.slice(0, 100) + "...";
+        cardIngredients.appendChild(ingredientItem);
+      }
+    });
+    const cardSteps = document.createElement("p");
+    cardSteps.classList.add("card-steps");
+    cardSteps.innerHTML = data[i].description.slice(0, 100) + "...";
+    card.append(cardImg, cardHead, cardInstructions);
+    cardHead.append(cardTitle, cardTimeBox);
+    cardTimeBox.append(cardClock, cardTimer);
+    cardInstructions.append(cardIngredients, cardSteps);
+    cardsContainer.appendChild(card);
+  }
 }
+createCards(recipes);
+// Recherche de la barre principale
+mainSearchBar.addEventListener("keyup", (event) => {
+  const mainSearchContent = event.target.value;
+  if (mainSearchContent.length >= 3) {
+    // Recherche par titre
+    const foundName = recipes
+      .filter((recipe) => recipe.name.includes(mainSearchContent))
+      .filter((recipe) => recipe);
+    // Recherche par ingredient
+    let foundIngredient = [];
+    for (const recipe of recipes) {
+      for (const ingredient of recipe.ingredients) {
+        if (ingredient.ingredient.includes(mainSearchContent)) {
+          foundIngredient.push(recipe);
+        }
+      }
+    }
+    // Recherche par description
+    const foundDescription = recipes
+      .filter((recipe) => recipe.description.includes(mainSearchContent))
+      .filter((recipe) => recipe);
+    // Actualisation des recettes affichees
+    foundFinal = [...foundName, ...foundIngredient, ...foundDescription];
+    createCards(foundFinal);
+  } else {
+    cardsContainer.innerHTML = "";
+    createCards(recipes);
+  }
+});
